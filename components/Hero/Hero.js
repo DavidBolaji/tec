1c66e327920637a1d278e84e6c1c073e09881bc5
David <odavidbolaji14@gmail.com>
2021-12-17 08:37:03 +0100

    adding mobile style for alignment

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -4,34 +4,12 @@ import HeroList from '../HeroList/HeroList';
 import Home from '../../assets/images/home.webp';
 import School from '../../assets/images/school.webp';
 import Community from '../../assets/images/community1.webp';
+import datas from '../../data/carou-data';
 
 
 import { useSelector, useDispatch } from 'react-redux';
 
 
-const datas = [
-    {
-        id: '01',
-        title: 'Every child needs personalized education',
-        text: 'We’re excited to help empower TEC parents and TEC students to be active and engaged partners in learning.',
-        image: Home,
-        page: 'home'
-    },
-    {
-        id: '02',
-        title: 'Schools are the greatest institutions',
-        text: 'At TEC, and with TEC IN EVERY SCHOOL innovation, our mission is to transform your vision and meet every student/pupil specific need.',
-        image: School,
-        page: 'school'
-    },
-    {
-        id: '03',
-        title: 'Tecworld, focus on center activities',
-        text: 'TEC IN EVERY COMMUNITY allow us to train and have seminars for companies, organizations, business owners etc.',
-        image: Community,
-        page: 'community'
-    }
-];
 
 const Hero = () => {
     const active = useSelector(state => state.themeReducer);
--- data/carou-data.js
+++ data/carou-data.js
@@ -1,29 +1,29 @@
 
-import Home from '../assets/images/home.png';
-import School from '../assets/images/school.png';
-import Community from '../assets/images/community.png';
+import Home from '../assets/images/home.webp';
+import School from '../assets/images/school.webp';
+import Community from '../assets/images/community1.webp';
 
 const datas = [
     {
         id: '01',
         title: 'Every child needs personalized education',
-        text: 'We’re excited to help empower TEC parents and TEC students to be active, engaged partners in learning, offering new ways to personalize how schools teach and inspire today’s students',
+        text: 'We’re excited to help empower TEC parents and TEC students to be active and engaged partners in learning.',
         image: Home,
-        page: 'tec-in-every-home'
+        page: 'home'
     },
     {
         id: '02',
         title: 'Schools are the greatest institutions',
-        text: 'At TEC, and with TEC IN EVERY SCHOOL innovation, our mission is to transform your vision and meet every student/pupil specific need, creating pathways to good success!',
+        text: 'At TEC, and with TEC IN EVERY SCHOOL innovation, our mission is to transform your vision and meet every student/pupil specific need.',
         image: School,
-        page: 'tec-in-every-school'
+        page: 'school'
     },
     {
         id: '03',
-        title: 'Tecworld Learning focus on center activities',
+        title: 'Tecworld, focus on center activities',
         text: 'TEC IN EVERY COMMUNITY allow us to train and have seminars for companies, organizations, business owners etc.',
         image: Community,
-        page: 'tec-in-every-community'
+        page: 'community'
     }
 ];
 
